c9b1c2692fcb232fd8ff97a0968fc1425742c0a5
Dunkyl <38872099+dunkyl@users.noreply.github.com>
2022-10-17 01:04:48 -0700

    py38

--- bedtime/common.py
+++ bedtime/common.py
@@ -1,15 +1,13 @@
-from typing import Callable, TypeAlias
+from typing import Callable, Optional
 import threading
 
-Action: TypeAlias = Callable[[], None]
-
 class _Listener():
-    on_sleep: Action
-    on_shutdown: Action
+    on_sleep: Callable[[], None]
+    on_shutdown: Callable[[], None]
 
     def _event_thread(self): raise NotImplementedError()
 
-    def __init__(self, *, on_sleep: Action|None=None, on_shutdown: Action|None=None):
+    def __init__(self, *, on_sleep: Optional[Callable[[], None]]=None, on_shutdown: Optional[Callable[[], None]]=None):
         self.on_sleep = on_sleep or (lambda: None)
         self.on_shutdown = on_shutdown or (lambda: None)
         self._thread = threading.Thread(target=self._event_thread, daemon=True)
